4a6aa6ebbe3752d80e1b1ef78ee10e5b97038bd0
vincent31545 <vincentlau31545@gmail.com>
2024-08-09 00:54:28 -0400

    changing the project to react

---
+++ app/README.md
@@ -0,0 +1,10 @@
+Here put info on how to run your program and a general description of each page 
+
+
+
+Home page:
+	has blah
+
+Menu page:
+	has images
+	json stuff
---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HomePage from './home_page/home_page.js';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
