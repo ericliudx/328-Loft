ed43799a1a0d8f031aec6de14c74175fe20dc7f3
Eric Liu <liufamily@MacBook-Pro-4.local>
2024-08-24 03:27:18 -0400

    home page menu favs section basically done

--- app/src/home_page/home-page-menu/home-page-menu.js
+++ app/src/home_page/home-page-menu/home-page-menu.js
@@ -1,7 +1,17 @@
 import "./home-page-menu.css";
 import HomePageMenuItem from "./home-page-menu-item/home-page-menu-item";
 
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 function HomePageMenu({image}) {
+
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/food-page');
+    };
+
     return (
         <div className="home-page-menu-container">
             <div className="home-page-menu-left">
@@ -21,10 +31,14 @@ function HomePageMenu({image}) {
                     </div>
                 </div>
 
+                <div className="home-page-menu-button-container">
+                    <button className="home-page-menu-view-full-menu" onClick = {handleClick}>
+                        <div className="home-page-menu-button-text">
+                            View our full menu
+                        </div>
+                    </button>
+                </div>
 
-                <button className="home-page-menu-view-full-menu">
-                    View our full menu
-                </button>
             </div>
             <img className="home-page-menu-right" src={image}></img>
         </div>
